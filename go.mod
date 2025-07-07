dc10dc29ba0855c245f2de292a9ddfcfad20b3b0
dontbel1eveme <vanyapopkov05@gmai.com>
2025-07-07 17:22:31 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/dontbel1eveme/bookingstsawler
+
+go 1.24.4
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/justinas/nosurf v1.2.0 // indirect
+)
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/dontbel1eveme/bookingstsawler/pkg/config"
+	"github.com/dontbel1eveme/bookingstsawler/pkg/models"
+	"github.com/dontbel1eveme/bookingstsawler/pkg/render"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home if the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
